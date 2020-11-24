ac0e9648b85d9bf555c0bbba63ff053994ece9a1
Michał Gleba <scranderpl@gmail.com>
2020-11-24 20:53:50 +0100

    Modified H&K to use symbols from the article. Added improved H&K algorithm for steganography.

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ import numpy as np
 import random
 from graphics import *
 import time
+import itertools
 
 x_size = int(input("Insert X size: "))
 y_size = int(input("Insert Y size: "))
 print("1. Recursive backtracker")
 print("2. Hunt and kill")
 print("3. Eller's algorithm")
+print("3. Hunt and kill (modified for work with steganography)")
 algorithm = int(input("Choose algorithm: "))
 STEP = 2
-CELL_SIZE = 50
-FRAME_RATE = 30
+CELL_SIZE = 40
+FRAME_RATE = 1
 
 
 
@@ -93,12 +95,12 @@ def print_maze_graph(win, cells):
                 pt2 = Point((x // 2 + 1) * CELL_SIZE-1, (y // 2 + 1) * CELL_SIZE-1)
                 rt = Rectangle(pt1, pt2)
                 if cells[x, y] == 'C':
-                    rt.setOutline(color_rgb(0, 255, 0))
-                    rt.setFill(color_rgb(0, 255, 0))
+                    #rt.setOutline(color_rgb(0, 255, 0))
+                    rt.setFill(color_rgb(0, 56, 0))
                     rt.draw(win)
                 else:
                     try:
-                        int(cells[x, y])
+                        str(cells[x, y])
                     except ValueError:
                         continue
                     pt1.move(CELL_SIZE/2, CELL_SIZE/2)
@@ -108,6 +110,7 @@ def print_maze_graph(win, cells):
                     label.setFill(color)
                     label.setSize(CELL_SIZE//3)
                     label.draw(win)
+
 
 
 def get_cell(cells, x, y):
@@ -457,6 +460,265 @@ def dfs_iterative_solver(win, maze, start_x, start_y, end_x, end_y):
 
     return None
 
+
+
+def random_start(cells, W = 0):
+    if W == 0:
+        wall_chosen = random.randint(1, 4)
+    else:
+        wall_chosen = W
+
+    if wall_chosen == 1:
+        rand_x = 1
+        rand_y = random.randint(1, y_size)
+    elif wall_chosen == 2:
+        rand_x = x_size
+        rand_y = random.randint(1, y_size)
+    elif wall_chosen == 3:
+        rand_x = random.randint(1, x_size)
+        rand_y = 1
+    else:
+        rand_x = random.randint(1, x_size)
+        rand_y = y_size
+
+    return rand_x, rand_y
+
+def get_cell_neighbours(cells, x, y, value):
+    neighbours = []
+    if x > 1:
+        neighbour = get_cell(cells, x-1, y)
+        if neighbour == value:
+            neighbours.append([x-1, y])
+    if x < x_size:
+        neighbour = get_cell(cells, x+1, y)
+        if neighbour == value:
+            neighbours.append([x+1, y])
+    if y > 1:
+        neighbour = get_cell(cells, x, y-1)
+        if neighbour == value:
+            neighbours.append([x, y-1])
+    if y < y_size:
+        neighbour = get_cell(cells, x, y+1)
+        if neighbour == value:
+            neighbours.append([x, y+1])
+    return neighbours
+
+def hunt_and_kill_fill(cells):
+    for y in range(0, cells.shape[1]):
+        for x in range(0, cells.shape[0]):
+            if x % 2 != 0 and y % 2 == 0:
+                cells[x, y] = 'W'
+            elif x % 2 == 0 and y % 2 != 0:
+                cells[x, y] = 'W'
+            else:
+                cells[x, y] = 'O'
+
+def hunt_and_kill_clear(cells):
+    for y in range(0, cells.shape[1]):
+        for x in range(0, cells.shape[0]):
+            cells[x, y] = ' '
+
+def hunt_and_kill_mod(win, maze, reuse = False):
+    def hunt_and_kill_scan(win, maze):
+        for y in range(1, y_size + 1):
+            for x in range(1, x_size + 1):
+                if get_cell(maze, x, y) == 'F':
+                    neighbours = get_cell_neighbours(maze, x, y, 'I')
+                    if not neighbours:
+                        continue
+                    return random.choice(neighbours)
+        return None, None
+
+    if reuse:
+        stop = True
+        # step required for maze reuse
+        for y in range(1, y_size + 1):
+            for x in range(1, x_size + 1):
+                if get_cell(maze, x, y) == 'F':
+                    neighbours = get_cell_neighbours(maze, x, y, 'I')
+                    if not neighbours:
+                        continue
+                    neigh_x, neigh_y = random.choice(neighbours)
+                    set_wall(maze, neigh_x, neigh_y, x, y, ' ')
+                    curr_x, curr_y = x, y
+
+                    stop = False
+                    break
+            if not stop:
+                break
+        if stop:
+            return
+
+    else:
+        curr_x = random.randint(1, x_size)
+        curr_y = random.randint(1, y_size)
+
+
+
+
+
+
+    while True:
+        #redraw(win, maze, FRAME_RATE)
+        set_cell(maze, curr_x, curr_y, 'I')
+        neighbours = get_cell_neighbours(maze, curr_x, curr_y, 'O')
+        for x, y in neighbours:
+            set_cell(maze, x, y, 'F')
+
+        neighbours = get_cell_neighbours(maze, curr_x, curr_y, 'F')
+
+        if not neighbours:
+            curr_x, curr_y = hunt_and_kill_scan(win, maze)
+            if not curr_x:
+                return
+            neighbours = get_cell_neighbours(maze, curr_x, curr_y, 'F')
+
+        neigh_x, neigh_y = random.choice(neighbours)
+        set_wall(maze, curr_x, curr_y, neigh_x, neigh_y, ' ')
+        curr_x, curr_y = neigh_x, neigh_y
+
+def hunt_and_kill_steg(win, maze):
+    # choose n start points
+    # order is important (set with seed)
+
+    # required for step 8
+    embeddable_cells = []
+
+    redraw(win, maze, FRAME_RATE)
+    input()
+
+    # step 2
+    start_points = []
+    paths = []
+    start_points.append(random_start(maze, W=2))
+    start_points.append(random_start(maze, W=4))
+
+    end_x, end_y = random_start(maze, W=1)
+
+    for start_x, start_y in start_points:
+        paths.append(bfs_solver(win, maze, start_x, start_y, end_x, end_y))
+
+
+
+    message = '010101010101'
+
+    redraw(win, maze, FRAME_RATE)
+    input()
+    # step 3
+    hunt_and_kill_fill(maze)
+    for path in paths:
+        prev_x, prev_y = None, None
+        for (x, y) in path:
+            if prev_x:
+                set_wall(maze, x, y, prev_x, prev_y, ' ')
+            set_cell(maze, x, y, 'I')
+            prev_x, prev_y = x, y
+
+    redraw(win, maze, FRAME_RATE)
+    input()
+    # step 4
+    for path in paths:
+        for (curr_x, curr_y) in path:
+            if curr_x == 1 or curr_x == x_size or curr_y == 1 or curr_y == y_size:
+                continue
+            neighbours = get_cell_neighbours(maze, curr_x, curr_y, 'O')
+            if len(neighbours) != 2:
+                continue
+            set_cell(maze, neighbours[0][0], neighbours[0][1], 'O0')
+            set_cell(maze, neighbours[1][0], neighbours[1][1], 'O1')
+            embeddable_cells.append((curr_x, curr_y))
+
+    redraw(win, maze, FRAME_RATE)
+    input()
+    # step 5
+    message_counter = 0
+    for path in paths:
+        for (curr_x, curr_y) in path:
+            if curr_x == 1 or curr_x == x_size or curr_y == 1 or curr_y == y_size:
+                continue
+            neighbour_o0 = get_cell_neighbours(maze, curr_x, curr_y, 'O0')
+            neighbour_o1 = get_cell_neighbours(maze, curr_x, curr_y, 'O1')
+            if neighbour_o0 == [] or neighbour_o1 == []:
+                continue
+            neighbour_o0 = neighbour_o0[0]
+            neighbour_o1 = neighbour_o1[0]
+
+
+            curr_bit = '0'
+            if message_counter < len(message):
+                curr_bit = message[message_counter]
+            if curr_bit == '0':
+                set_cell(maze, neighbour_o0[0], neighbour_o0[1], 'D')
+                set_cell(maze, neighbour_o1[0], neighbour_o1[1], 'I')
+                set_wall(maze, curr_x, curr_y, neighbour_o1[0], neighbour_o1[1], ' ')
+            else:
+                set_cell(maze, neighbour_o1[0], neighbour_o1[1], 'D')
+                set_cell(maze, neighbour_o0[0], neighbour_o0[1], 'I')
+                set_wall(maze, curr_x, curr_y, neighbour_o0[0], neighbour_o0[1], ' ')
+            message_counter = message_counter + 1
+
+    redraw(win, maze, FRAME_RATE)
+    input()
+    # step 6
+    for (curr_x, curr_y) in itertools.product(range(1,x_size+1),range(1,y_size+1)):
+        if get_cell(maze, curr_x, curr_y) != 'I':
+            continue
+        neighbours = get_cell_neighbours(maze, curr_x, curr_y, 'O')
+        for neigh_x, neigh_y in neighbours:
+            set_cell(maze, neigh_x, neigh_y, 'F')
+
+    redraw(win, maze, FRAME_RATE)
+    input()
+    while True:
+        # step 7
+        hunt_and_kill_mod(win, maze, reuse=True)
+
+        # step 8
+        d_exist = False
+        for (curr_x, curr_y) in itertools.product(range(1, x_size + 1), range(1, y_size + 1)):
+
+            if get_cell(maze, curr_x, curr_y) != 'D':
+                continue
+
+            neighbours = get_cell_neighbours(maze, curr_x, curr_y, 'I')
+
+            unemb_i_exist = False
+            for x, y in neighbours:
+                if (x, y) in embeddable_cells:
+                    continue
+                unemb_i_exist = True
+                found_x, found_y = curr_x, curr_y
+                unemb_x, unemb_y = x, y
+                break
+
+            if not unemb_i_exist:
+                continue
+            d_exist = True
+            break
+
+
+
+        if d_exist:
+            set_wall(maze, found_x, found_y, unemb_x, unemb_y, ' ')
+            set_cell(maze, found_x, found_y, 'I')
+            neighbours = get_cell_neighbours(maze, found_x, found_y, 'O')
+            for x, y in neighbours:
+                set_cell(maze, x, y, 'F')
+        else:
+            break
+
+
+
+    # end (step 9)
+    start_iter = 0
+    for start_x, start_y in start_points:
+        set_cell(maze, start_x, start_y, 'S' + str(start_iter))
+        start_iter = start_iter + 1
+
+    set_cell(maze, end_x, end_y, 'E')
+
+
+# raw maze, filled with 0's
 maze = np.zeros(shape=(x_size*2-1, y_size*2-1)).astype(str)
 
 win = GraphWin("ss", x_size*CELL_SIZE+1, y_size*CELL_SIZE+1, autoflush=False)
@@ -473,34 +735,49 @@ elif algorithm == 2:
     hunt_and_kill(win, maze)
 elif algorithm == 3:
     eller(win, maze)
+elif algorithm == 4:
+    hunt_and_kill_clear(maze)
+    hunt_and_kill_fill(maze)
+
+    #x_start, y_start = random_start(maze)
+    #set_cell(maze, x_start, y_start, 'I')
+    hunt_and_kill_mod(win, maze)
+
+    hunt_and_kill_steg(win, maze)
 
 redraw(win, maze, FRAME_RATE)
 
 print("A* start solution:")
 start = time.time()
-print(astar_solver(win, maze, 1, 1, x_size, y_size))
+print(astar_solver(win, maze, 5, 5, x_size, y_size))
 end = time.time()
 print("elapsed time: {} sec.".format(end - start))
 
 print("BFS solution:")
 start = time.time()
-print(bfs_solver(win, maze, 1, 1, x_size, y_size))
+print(bfs_solver(win, maze, 5, 5, x_size, y_size))
 end = time.time()
 print("elapsed time: {} sec.".format(end - start))
 
 print("DFS solution:")
 start = time.time()
-print(dfs_solver(win, maze, 1, 1, x_size, y_size))
+print(dfs_solver(win, maze, 5, 5, x_size, y_size))
 end = time.time()
 print("elapsed time: {} sec.".format(end - start))
 
 print("iterative DFS solution:")
 start = time.time()
-print(dfs_iterative_solver(win, maze, 1, 1, x_size, y_size))
+path = dfs_iterative_solver(win, maze, 5, 5, x_size, y_size)
+print(path)
 end = time.time()
 print("elapsed time: {} sec.".format(end - start))
 
+#for (x, y) in path:
+#    set_cell(maze, x, y, 'C')
+
+redraw(win, maze, FRAME_RATE)
 
 win.getMouse()
 win.close()
 
+# processing - nakladka na pythona
